01acbdd30857e749b7b587748032f8d99bc4a571
utku-13 <ozerutku13@gmail.com>
2025-04-03 15:02:50 +0300

    added weather data

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-xml_file = "/Users/utkuozer/Desktop/DSA Proje/DSA-210-Project/export.xml"  
-tree = ET.parse(xml_file)
-root = tree.getroot()
+from wwo_hist import retrieve_hist_data
+
+# xml_file = "/Users/utkuozer/Desktop/DSA Proje/DSA-210-Project/export.xml"  
+# tree = ET.parse(xml_file)
+# root = tree.getroot()
 
 # Extracting my sleep data
-sleep_data = [] 
-for record in root.findall("Record"):
-    if record.attrib.get("type") == "HKCategoryTypeIdentifierSleepAnalysis":
-        # since it is very large dataset we will search it
-        # with specific "HKCategoryTypeIdentifierSleepAnalysis" keyword.
-        sleep_data.append({
-            "start_time": record.attrib.get("startDate"),
-            "end_time": record.attrib.get("endDate"),
-            "value": record.attrib.get("value")
-        })
+# sleep_data = [] 
+# for record in root.findall("Record"):
+#     if record.attrib.get("type") == "HKCategoryTypeIdentifierSleepAnalysis":
+#         # since it is very large dataset we will search it
+#         # with specific "HKCategoryTypeIdentifierSleepAnalysis" keyword.
+#         sleep_data.append({
+#             "start_time": record.attrib.get("startDate"),
+#             "end_time": record.attrib.get("endDate"),
+#             "value": record.attrib.get("value")
+#         })
 
 # Converting my sleep data to CSV
-sleep_df = pd.DataFrame(sleep_data)
-sleep_df.to_csv("sleep_data.csv", index=False)
-print("Sleep data saved to sleep_data.csv")
+# sleep_df = pd.DataFrame(sleep_data)
+# sleep_df.to_csv("sleep_data.csv", index=False)
+# print("Sleep data saved to sleep_data.csv")
+
+# will set an API KEY from openweather and will os for security.
+
+
+# printing my sleep data.
+df = pd.read_csv("DSA-210-Project/sleep_data.csv")
+print(df.tail())
 
